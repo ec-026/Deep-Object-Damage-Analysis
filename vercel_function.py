5e299994b69e4e69332dfac747a4066b6a6d91db
Tejas Singh <notnormalcoder@gmail.com>
2023-09-18 16:13:54 +0530

    Delete files:

--- vercel_function.py
+++
@@ -1,8 +0,0 @@
-# vercel_function.py
-from flask import Flask
-from webapp import app  # Import your Flask app from webapp.py
-
-
-def handler(request, response):
-    # You can handle requests here using your Flask app
-    return app(request.environ, response)
